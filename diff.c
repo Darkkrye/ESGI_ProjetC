83ee680840657639bd6ed06021fb1a37cf04af98
NicoK78 <nicolas.kervoern@gmail.com>
2015-11-23 23:34:03 +0100

    add ignore-space

--- diff.c
+++ diff.c
@@ -9,6 +9,7 @@ int my_diff_brief(char* pArray, char* pArray2);
 int my_diff_report_identical_files(char* pArray, char* pArray2);
 void my_diff_help();
 int my_diff_ignore_case(char* pArray1, char* pArray2);
+int my_diff_ignore_space(char* pArray1, char* pArray2);
 
 int main(int argc, char** argv){
     
@@ -28,16 +29,20 @@ int main(int argc, char** argv){
 		}
     }
 	
-	if (argv[1][0] == 45 && argv[1][1] == 113) {
+	if (strcmp(argv[1], "-q") == 0  || strcmp(argv[1], "--brief") == 0) {
 		my_diff_brief(argv[2], argv[3]);
-	} else if (argv[1][0] == 45 && argv[1][1] == 115) {
+	} else if (strcmp(argv[1], "-s") == 0 || strcmp(argv[1], "--report-identical-files") == 0) {
 		my_diff_report_identical_files(argv[2], argv[3]);
-    // } else if (argv[1][0] == 45 && argv[1][1] == ) {
-
-	} else if (argv[1][0] == 45 && argv[1][1] == 45 && argv[1][2] == 104) {
+    } else if (strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "--ignore-case") == 0) {
+        my_diff_ignore_case(argv[2], argv[3]);
+    } else if (strcmp(argv[1], "-w") == 0 || strcmp(argv[1], "--ignore-all-space") == 0) {
+        my_diff_ignore_space(argv[2], argv[3]);
+	} else if (strcmp(argv[1], "--help") == 0) {
 		my_diff_help();
+    } else if (strcmp(argv[1], "--normal") == 0) {
+        my_diff(argv[2], argv[3]);
 	} else {
-		my_diff_ignore_case(argv[1], argv[2]);
+		my_diff(argv[1], argv[2]);
 	}
 }
 
@@ -218,6 +223,181 @@ int my_diff_ignore_case(char* pArray1, char* pArray2) {
     free(noCase2);
 
 	return 0;
+}
+
+int my_diff_ignore_space(char* pArray1, char* pArray2) {
+    int j=0;
+    
+    FILE * file1=fopen(pArray1,"r+");
+    FILE * file2=fopen(pArray2,"r+");
+
+    if(file1 == NULL || file2 == NULL)
+        return 1;
+
+
+    // INIT TAB1
+    char** tab1 = (char**)malloc((MAX_SIZE)*sizeof(char*));
+    char ** tmp1 = tab1;
+    char** noSpace1 = (char**)malloc((MAX_SIZE)*sizeof(char*));
+    char* readThrough1 = (char*)malloc((MAX_SIZE)*sizeof(char));
+    int tabSize1 = 0;
+    int x;    
+    while(fgets(readThrough1, MAX_SIZE, file1) != NULL) {
+        tmp1[tabSize1] = (char*)malloc((strlen(readThrough1))*sizeof(char));
+        noSpace1[tabSize1] = (char*)malloc((strlen(readThrough1))*sizeof(char));
+        strcpy(noSpace1[tabSize1], readThrough1);
+        for(x = 0 ; strcmp(&noSpace1[tabSize1][x], "\0") != 0 ; x++) {
+            if(noSpace1[tabSize1][x] != 32) {
+                tmp1[tabSize1][x] = noSpace1[tabSize1][x];
+            }
+        }
+        tabSize1++;
+    }
+    
+    // INIT TAB2
+    char** tab2 = (char**)malloc((MAX_SIZE)*sizeof(char*));
+    char ** tmp2 = tab2;
+    char** noSpace2 = (char**)malloc((MAX_SIZE)*sizeof(char*));
+    char* readThrough2=(char*)malloc((MAX_SIZE)*sizeof(char));
+    int tabSize2=0;
+    j = 0;
+    while(fgets(readThrough2, MAX_SIZE, file2) != NULL) {
+        tmp2[tabSize2] = (char*)malloc((strlen(readThrough2))*sizeof(char));
+        noSpace2[tabSize2] = (char*)malloc((strlen(readThrough2))*sizeof(char));
+        strcpy(noSpace2[tabSize2], readThrough2);
+        for(x = 0 ; strcmp(&noSpace2[tabSize2][x], "\0") != 0 ; x++) {
+            if(noSpace2[tabSize2][x] != 32) {
+                tmp2[tabSize2][x] = noSpace2[tabSize2][x];
+            }
+        }
+        tabSize2++;
+    }
+
+    // FILES COMPARISON
+    int i = 0;
+    j = 0;
+    int j2 = 0, i2 = 0;
+    int isLinesAdded = 0, isLinesDeleted = 0, isLinesReplaced = 0;
+
+    while(i < tabSize1 && j < tabSize2) {
+        if(!strcmp(tab1[i], tab2[j])) {
+            i++;
+            j++;
+        } else {
+            j2 = j;
+            isLinesAdded = 0;
+            while (j < tabSize2) {
+
+                if(!strcmp(tab1[i], tab2[j]))
+                {
+                    if(j == j2 + 1) {
+                        printf("%da%d\n> %s", i, j, noSpace2[j2]);
+                        isLinesAdded = 1;
+                    } else {
+                        printf("%da%d,%d\n", i, j2+1, j);
+                        isLinesAdded = 1;
+                        for( ; j2 < j ; j2++) {
+                            printf("> %s", noSpace2[j2]);
+                        }
+                    }
+                    if(isLinesAdded) {
+                        j2++;
+                        break;
+                    }
+                }
+                j++;
+            }
+
+            if(!isLinesAdded) {
+                j = j2;
+                i2 = i;
+                isLinesDeleted = 0;
+                while(i < tabSize1) {
+                    if(!strcmp(tab1[i], tab2[j])) {
+                        if (i == i2 + 1) {
+                            printf("%dd%d\n< %s", i, j, noSpace1[i2]);
+                            isLinesDeleted = 1;
+                        } else {
+                            printf("%d,%dd%d\n", i2+1, i, j);
+                            isLinesDeleted = 1;
+                            for( ; i2 < i ; i2++) {
+                                printf("< %s", noSpace1[i2]);
+                            }
+                        }
+
+                        if(isLinesDeleted) {
+                            i2++;
+                            break;
+                        }
+                    }
+                    i++;
+                }
+
+                if(!isLinesDeleted) {
+                    i = i2;
+                    isLinesReplaced = 0;
+                    while(i < tabSize1) {
+                        j = j2;
+                        while(j < tabSize2) {
+                            if(!strcmp(tab1[i], tab2[j])) {
+                                if(i2 + 1 == i && j2 + 1 == j)
+                                    printf("%dc%d\n", i, j);
+                                else if (i2 + 1 == i && j2 + 1 != j)
+                                    printf("%dc%d,%d\n", i2+1, j2+1, j);
+                                else if(i2 + 1 != i && j2 + 1 == j)
+                                    printf("%d,%dc%d\n", i2+1, i, j2+1);
+                                else 
+                                    printf("%d,%dc%d,%d\n", i2+1, i, j2+1, j);
+
+                                for (; i2 < i; i2++) {
+                                    printf("< %s", noSpace1[i2]);
+                                }
+                                printf("---\n");
+                                for(; j2 < j; j2++) {
+                                    printf("> %s", noSpace2[j2]);
+                                }
+                                isLinesReplaced = 1;
+                                break;
+                            }
+                            j++;
+                        }
+                        if(isLinesReplaced)
+                            break;
+                        i++;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    if(i >= tabSize1 && j < tabSize2) {
+        if(j + 1 == tabSize2) {
+            printf("%da%d\n> %s", i, j+1, noSpace2[j]);
+        } else {
+            printf("%da%d,%d\n", i, j+1, tabSize2);
+            for(; j < tabSize2 ; j++) {
+                printf("> %s", noSpace2[j]);
+            }
+        }
+    } else if(i < tabSize1 && j >= tabSize2) {
+        if(i + 1 == tabSize1) {
+            printf("%dd%d\n< %s", i, j+1, noSpace1[i]);
+        } else {
+            printf("%d,%da%d\n", i + 1, tabSize1, j);
+            for(; i < tabSize1 ; i++) {
+                printf("< %s", noSpace1[i]);
+            }
+        }
+    }
+
+    free(tab1);
+    free(tab2);
+    free(noSpace1);
+    free(noSpace2);
+
+    return 0;
 }
 
 int my_diff(char* pArray1, char* pArray2) {
